b26af7b3e7190c8ea3e6d909f01ac72484e694d7
Alexey ALexandrov <aleksandrov1988@gmail.com>
2019-11-22 20:21:50 +0300

    Task03

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,25 @@ class User < ApplicationRecord
 
   validates :name, length: {in: 2..100}
   validates :login, presence: true, length: {in: 2..100}, uniqueness: {case_sensetive: false}
+  validates :password, length: {minimum: 6}, if: :should_validate_password?
+  validate :check_password
 
   scope :ordering, -> { order(:login) }
 
   def edit_by?(current_user)
     self == current_user || current_user&.admin?
+  end
+
+  def should_validate_password?
+    new_record? || password_changed?
+  end
+
+  private
+
+  def check_password
+    return unless should_validate_password?
+    errors.add(:password, 'должен содержать цифру') if password !~ /\d/
+    errors.add(:password, 'должен содержать строчную латинскую буквы') if password !~ /[a-z]/
+    errors.add(:password, 'должен содержать прописную латинскую буквы') if password !~ /[A-Z]/
   end
 end
