0ad68046f608ee4b4df69e16ed336f5db79f5fb6
Alexey ALexandrov <aleksandrov1988@gmail.com>
2019-11-22 20:27:38 +0300

    Task08

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  def show_post_body(post)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown.render(post.body)
+  end
 end
